e88919b9d7d2cc123c73e12d0e6fc810f6718ce3
celsius narhwal <hello@celsiusnarhwal.dev>
2022-12-25 06:36:16 -0500

    update plugin.py

--- src/poetry_sort/plugin.py
+++ src/poetry_sort/plugin.py
@@ -8,7 +8,19 @@ from poetry.console.application import Application
 from poetry.console.commands.add import AddCommand
 from poetry.console.commands.command import Command
 from poetry.console.commands.init import InitCommand
+from poetry.core.toml import TOMLFile
 from poetry.plugins.application_plugin import ApplicationPlugin
+
+
+def get_plugin_config(poetry_file: TOMLFile):
+    config = {
+        "sort-python": False,
+        "format": True,
+    }
+
+    config.update(poetry_file.read().get("tool", {}).get("sort", {}).get("config", {}))
+
+    return config
 
 
 def sort_dependencies(cmd: Command, include: list, exclude: list, only: list):
@@ -29,7 +41,7 @@ def sort_dependencies(cmd: Command, include: list, exclude: list, only: list):
     def sort(dependencies: dict) -> dict:
         return {k: v for k, v in (sorted(dependencies.items(), key=lambda dep: Dependency(*dep)))}
 
-    sort_confg = cmd.poetry.file.read().get("tool", {}).get("sort", {}).get("config", {})
+    sort_confg = get_plugin_config(cmd.poetry.file)
 
     pyproject = cmd.poetry.file.read()
     poetry = pyproject["tool"]["poetry"]
@@ -75,20 +87,22 @@ class PoetrySortCommand(Command):
         )
 
 
-def factory():
-    return PoetrySortCommand()
-
-
 # noinspection PyMethodMayBeStatic,PyUnboundLocalVariable
 class PoetrySortPlugin(ApplicationPlugin):
     def activate(self, application: Application) -> None:
-        application.command_loader.register_factory("sort", factory)
+        application.command_loader.register_factory("sort", self.sort_command_factory)
         application.event_dispatcher.add_listener(TERMINATE, self.sort_dependencies)
+
+    @staticmethod
+    def sort_command_factory():
+        return PoetrySortCommand()
 
     def sort_dependencies(self, event: ConsoleTerminateEvent, event_name: str, dispatcher: EventDispatcher) -> None:
         command = event.command
 
-        if not isinstance(command, (AddCommand, InitCommand)):
+        triggers = (AddCommand, InitCommand)
+
+        if not isinstance(command, triggers):
             return
 
         if isinstance(command, InitCommand):
